13d31bf286ed1a5604bf80b7ea7e719c96c9fc5f
Leonid Mamchenkov <leonid@mamchenkov.net>
2015-06-16 23:13:03 +0300

    Removed unnecessary require_once

--- tests/DotenvTest.php
+++ tests/DotenvTest.php
@@ -10,7 +10,6 @@ class DotenvTest extends PHPUnit_Framework_TestCase {
 	 * Check that Dotenv is loaded by composer
 	 */
 	public function testDotenvClassLoaded() {
-		require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 		$this->assertTrue(class_exists('Dotenv'), 'Dotenv class is not loaded');
 	}
 }
