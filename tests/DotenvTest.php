faef09d4f45d530b94997febe0e797751b07e313
Leonid Mamchenkov <leonid@mamchenkov.net>
2015-06-17 01:28:38 +0300

    Test for .env.example presence

--- tests/DotenvTest.php
+++ tests/DotenvTest.php
@@ -12,4 +12,13 @@ class DotenvTest extends PHPUnit_Framework_TestCase {
 	public function testDotenvClassLoaded() {
 		$this->assertTrue(class_exists('Dotenv'), 'Dotenv class is not loaded');
 	}
+
+	/**
+	 * Check that .env.example file exists
+	 */
+	public function testDotenvExampleFileExists() {
+		$file = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '.env.example';
+		$this->assertTrue(file_exists($file), ".env.example file does not exist [$file]");
+	}
+
 }
