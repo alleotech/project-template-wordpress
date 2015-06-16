e3126cecde8eaa361d0a5449a17e4415291822e4
Leonid Mamchenkov <leonid@mamchenkov.net>
2015-06-17 02:17:14 +0300

    Reorganizing tests (broken)

---
+++ tests/Environment/DotenvTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Dotenv Test
+ * 
+ * @author Leonid Mamchenkov <l.mamchenkov@qobo.biz>
+ */
+class DotenvTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Check that Dotenv is loaded by composer
+	 */
+	public function testDotenvClassLoaded() {
+		$this->assertTrue(class_exists('Dotenv'), 'Dotenv class is not loaded');
+	}
+
+}
--- tests/Unit/DotenvTest.php
+++ tests/Unit/DotenvTest.php
@@ -7,17 +7,10 @@
 class DotenvTest extends PHPUnit_Framework_TestCase {
 
 	/**
-	 * Check that Dotenv is loaded by composer
-	 */
-	public function testDotenvClassLoaded() {
-		$this->assertTrue(class_exists('Dotenv'), 'Dotenv class is not loaded');
-	}
-
-	/**
 	 * Check that .env.example file exists
 	 */
 	public function testDotenvExampleFileExists() {
-		$file = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '.env.example';
+		$file = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '.env.example';
 		$this->assertTrue(file_exists($file), ".env.example file does not exist [$file]");
 	}
 
