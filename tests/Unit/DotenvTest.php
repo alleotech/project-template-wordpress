c1a4f9213935e4b845bd7d2dcbc7c30176b5c168
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2015-06-18 15:41:06 +0300

    Updated project-template to v1.0.11

---
+++ tests/Unit/DotenvTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace Tests\Unit;
+
+use \Dotenv;
+/**
+ * Dotenv Test
+ * 
+ * @author Leonid Mamchenkov <l.mamchenkov@qobo.biz>
+ */
+class DotenvTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Provide .env file locations
+	 */
+	public function dotEnvFilesProvider() {
+		return array(
+			'.env.example' => array(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR, '.env.example'),
+			'.env'         => array(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR, '.env'),
+		);
+	}
+
+	/**
+	 * Check that the file exists
+	 * 
+	 * @dataProvider dotEnvFilesProvider
+	 */
+	public function testDotenvExampleFileExists($folder, $file) {
+		$this->assertFileExists($folder . $file);
+	}
+
+	/**
+	 * Check that we can parse the file
+	 * 
+	 * @dataProvider dotEnvFilesProvider
+	 */
+	public function testDotenvExampleFileIsParseable($folder, $file) {
+		try {
+			Dotenv::load($folder, $file);
+		}
+		catch (\Exception $e) {
+			$this->fail("Failed to parse file " . $file . " in " . $folder . " : " . $e->getMessage());
+		}
+	}
+}
