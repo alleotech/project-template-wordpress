3802369e16974bd722f5d3946770f42387691465
unknown <Stelios@.(none)>
2015-10-17 10:39:53 +0300

    added 'FUNCTIONS_DIR' constant for functions directory and script to include all php files in directory

--- custom-themes/custom/functions.php
+++ custom-themes/custom/functions.php
@@ -10,4 +10,27 @@
 	if (!defined('IMAGES_URI')) {
 		define('IMAGES_URI', get_stylesheet_directory_uri() . '/images/');
 	}
+	
+	//Theme's functions directory
+	if (!defined('FUNCTIONS_DIR')) {
+		define('FUNCTIONS_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR);
+	}
+	
+	/**
+	 * Load all includes which are placed in theme's folder
+	 *
+	 * @return void
+	 */
+	if ( ! function_exists( 'loadFunctions' ) ):
+		function loadFunctions() {
+			$it = new DirectoryIterator(FUNCTIONS_DIR);
+			$it = new RegexIterator($it, '#.php$#');
+			foreach ($it as $include) {
+				if ($include->isReadable()) {
+					require_once($include->getPathname());
+				}
+			}
+		}
+		loadFunctions();
+	endif;
 ?>
