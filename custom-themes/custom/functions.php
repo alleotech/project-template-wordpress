fc08d1cfb997828fa83d93c1380e401c04baca4e
unknown <Stelios@.(none)>
2015-10-17 10:44:15 +0300

    Removed enqueue_parent_theme_style() from functions.php and created a separate file in functions folder

--- custom-themes/custom/functions.php
+++ custom-themes/custom/functions.php
@@ -1,11 +1,4 @@
 <?php
-	# Read more about child theme development at:
-	# http://codex.wordpress.org/Child_Themes
-	add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
-	function enqueue_parent_theme_style() {
-		wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css' );
-	}
-
 	//Theme's images URI
 	if (!defined('IMAGES_URI')) {
 		define('IMAGES_URI', get_stylesheet_directory_uri() . '/images/');
