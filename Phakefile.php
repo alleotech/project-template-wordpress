9fbed2e18bd933ad6a7d7aaa7eec32351b9e46f5
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-07-22 11:19:22 +0300

    Save project version that is being deployed
    * Save project version into build/version right after builder:init
    * Save project version into build/version.ok after app:install and app:update
    * Backup build/version and build/version.ok before overwriting them
    * Use GIT_BRANCH, or git hash, or 'Unknown' for project version

--- Phakefile.php
+++ Phakefile.php
@@ -1,6 +1,33 @@
 <?php
 require_once 'vendor/qobo/phake-builder/Phakefile.php';
 
+function getProjectVersion($app = null) {
+	$result = null;
+
+	// If we have $app variables, try to figure out version
+	if (!empty($app)) {
+		// Use GIT_BRANCH variable ...
+		$result = getValue('GIT_BRANCH', $app);
+		// ... if empty, use git hash
+		if (empty($result)) {
+			try {
+				$git = new \PhakeBuilder\Git(getValue('SYSTEM_COMMAND_GIT', $app));
+				$result = doShellCommand($git->getCurrentHash(), null, true);
+			}
+			catch (\Exception $e) {
+				// ignore
+			}
+		}
+	}
+
+	// ... if empty, use default
+	if (empty($result)) {
+		$result = 'Unknown';
+	}
+
+	return $result;
+}
+
 group('app', function() {
 
 	desc('Install application');
@@ -25,5 +52,36 @@ group('app', function() {
 		printInfo("Task: app:remove (Update application)");
 	});
 	task('remove', ':dotenv:delete');
+
+	//
+	// Save version that we are deploying, both before and after
+	//
+
+	after(':builder:init', function($app) {
+		$version = getProjectVersion($app);
+		// Save the version that we are deploying
+		if (file_exists('build/version')) {
+			rename('build/version', 'build/version.bak');
+		}
+		file_put_contents('build/version', $version);
+	});
+
+	after('install', function($app) {
+		$version = getProjectVersion($app);
+		// Save the version that we have deployed
+		if (file_exists('build/version.ok')) {
+			rename('build/version.ok', 'build/version.ok.bak');
+		}
+		file_put_contents('build/version.ok', $version);
+	});
+
+	after('update', function($app) {
+		$version = getProjectVersion($app);
+		// Save the version that we have deployed
+		if (file_exists('build/version.ok')) {
+			rename('build/version.ok', 'build/version.ok.bak');
+		}
+		file_put_contents('build/version.ok', $version);
+	});
 
 });
