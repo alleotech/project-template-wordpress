6544e69ccc6e7b6c74b485c4c714e8c3f36c4769
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-02-02 10:28:52 +0200

    Coding style fixes

--- webroot/custom-themes/custom/lib/functions/require-login.php
+++ webroot/custom-themes/custom/lib/functions/require-login.php
@@ -13,12 +13,13 @@
  *
  * @return void
  */
-function redirect_non_logged_users_to_login_page() {
-	if ( getenv( 'REQUIRE_LOGIN' ) ) {
-		global $pagenow;
-		if ( ! is_user_logged_in() && 'wp-login.php' !== $pagenow ) {
-			wp_safe_redirect( wp_login_url() );
-		}
-	}
+function redirect_non_logged_users_to_login_page()
+{
+    if (getenv('REQUIRE_LOGIN')) {
+        global $pagenow;
+        if (! is_user_logged_in() && 'wp-login.php' !== $pagenow) {
+            wp_safe_redirect(wp_login_url());
+        }
+    }
 }
-add_action( 'wp', 'redirect_non_logged_users_to_login_page' );
+add_action('wp', 'redirect_non_logged_users_to_login_page');
