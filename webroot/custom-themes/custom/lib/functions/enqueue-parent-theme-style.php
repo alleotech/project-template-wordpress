1810da078537d3f87b20faeb53592e42be8eacec
Leonidas Christodoulou <l.christodoulou@qobo.biz>
2017-02-24 11:47:06 +0200

    Added smoke v3.1.1 (task #3402)

--- webroot/custom-themes/custom/lib/functions/enqueue-parent-theme-style.php
+++ webroot/custom-themes/custom/lib/functions/enqueue-parent-theme-style.php
@@ -23,6 +23,7 @@ function enqueue_parent_theme_style() {
 	wp_enqueue_style( 'bootstrap-min-css', INCLUDES_DIR . 'bootstrap-3.3.7-dist/css/bootstrap.min.css' );
 	wp_enqueue_style( 'bootstrap-theme-min-css', INCLUDES_DIR . 'bootstrap-3.3.7-dist/css/bootstrap-theme.min.css' );
 	wp_enqueue_style( 'font-awesome-min-css', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'smoke-min-css', INCLUDES_DIR . 'smoke-v3.1.1/css/smoke.min.css' );
 }
 
 
@@ -37,8 +38,10 @@ function enqueue_parent_theme_script() {
 	wp_register_script( 'jquery-min-js', INCLUDES_DIR . 'jquery/jquery-1.12.4.min.js' , array(), false, true );
 	wp_register_script( 'jquery-ui-min-js', INCLUDES_DIR . 'jquery-ui-1.12.1/jquery-ui.min.js', array(), false, true );
 	wp_register_script( 'bootstrap-min-js', INCLUDES_DIR . 'bootstrap-3.3.7-dist/js/bootstrap.min.js', array(), false, true );
+	wp_register_script( 'smoke-min-js', INCLUDES_DIR . 'smoke-v3.1.1/js/smoke.min.js', array(), false, true );
 
 	wp_enqueue_script( 'jquery-min-js' );
 	wp_enqueue_script( 'jquery-ui-min-js' );
 	wp_enqueue_script( 'bootstrap-min-js' );
+	wp_enqueue_script( 'smoke-min-js' );
 }
