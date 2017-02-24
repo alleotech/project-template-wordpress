43c0812d8baf2369cafe85a6a526f779bf9c3304
Leonidas Christodoulou <l.christodoulou@qobo.biz>
2017-02-24 12:47:49 +0200

    Change directory structure of css/js (task #3402)

--- webroot/custom-themes/custom/lib/functions/enqueue-parent-theme-style.php
+++ webroot/custom-themes/custom/lib/functions/enqueue-parent-theme-style.php
@@ -20,10 +20,10 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
 function enqueue_parent_theme_style() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-custom', get_stylesheet_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'bootstrap-min-css', INCLUDES_DIR . 'bootstrap-3.3.7-dist/css/bootstrap.min.css' );
-	wp_enqueue_style( 'bootstrap-theme-min-css', INCLUDES_DIR . 'bootstrap-3.3.7-dist/css/bootstrap-theme.min.css' );
-	wp_enqueue_style( 'font-awesome-min-css', INCLUDES_DIR . 'font-awesome-4.7.0/css/font-awesome.min.css' );
-	wp_enqueue_style( 'smoke-min-css', INCLUDES_DIR . 'smoke-v3.1.1/css/smoke.min.css' );
+	wp_enqueue_style( 'bootstrap-min-css', INCLUDES_DIR . 'bootstrap/3.3.7/css/bootstrap.min.css' );
+	wp_enqueue_style( 'bootstrap-theme-min-css', INCLUDES_DIR . 'bootstrap/3.3.7/css/bootstrap-theme.min.css' );
+	wp_enqueue_style( 'font-awesome-min-css', INCLUDES_DIR . 'font-awesome/4.7.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'smoke-min-css', INCLUDES_DIR . 'smoke/3.1.1/css/smoke.min.css' );
 }
 
 
@@ -36,9 +36,9 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_script' );
  */
 function enqueue_parent_theme_script() {
 	wp_register_script( 'jquery-min-js', INCLUDES_DIR . 'jquery/jquery-1.12.4.min.js' , array(), false, true );
-	wp_register_script( 'jquery-ui-min-js', INCLUDES_DIR . 'jquery-ui-1.12.1/jquery-ui.min.js', array(), false, true );
-	wp_register_script( 'bootstrap-min-js', INCLUDES_DIR . 'bootstrap-3.3.7-dist/js/bootstrap.min.js', array(), false, true );
-	wp_register_script( 'smoke-min-js', INCLUDES_DIR . 'smoke-v3.1.1/js/smoke.min.js', array(), false, true );
+	wp_register_script( 'jquery-ui-min-js', INCLUDES_DIR . 'jquery-ui/1.12.1/jquery-ui.min.js', array(), false, true );
+	wp_register_script( 'bootstrap-min-js', INCLUDES_DIR . 'bootstrap/3.3.7/js/bootstrap.min.js', array(), false, true );
+	wp_register_script( 'smoke-min-js', INCLUDES_DIR . 'smoke/3.1.1/js/smoke.min.js', array(), false, true );
 
 	wp_enqueue_script( 'jquery-min-js' );
 	wp_enqueue_script( 'jquery-ui-min-js' );
