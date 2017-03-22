a15833dc26a9dd5ba8d2f514a939b2284c846e07
Leonidas Christodoulou <l.christodoulou@qobo.biz>
2017-03-22 13:22:28 +0200

    Adds get_iages_by_category_slug helper function (task #3535)

--- webroot/custom-themes/custom/lib/functions/helper-functions.php
+++ webroot/custom-themes/custom/lib/functions/helper-functions.php
@@ -22,3 +22,32 @@ function redirect_non_logged_users_to_login_page() {
 	}
 }
 add_action( 'wp', 'redirect_non_logged_users_to_login_page' );
+
+/**
+ * Gets images by gategory slug when wp-media-library-categories plugin is enabled
+ *
+ * @param string $slug Slug name of a category image group.
+ * @param int $no_of_images Number of images filter.
+ * @return boolen/WP_Post Object $result.
+ */
+function get_images_by_gategory_slug( $slug, $no_of_images = 1 ) {
+
+	$category = get_category_by_slug( $slug );
+
+	if ( ! $category ) {
+		return false;
+	}
+
+	$category_id = $category->term_id;
+
+	$args = array(
+		'post_type'   => 'attachment',
+		'numberposts' => $no_of_images,
+		'post_status' => null,
+		'category'    => $category_id,
+	);
+
+	$result = get_posts( $args );
+
+	return $result;
+}
