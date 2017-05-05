7f497c0056782bb7be43e8e02da1ffc0e758aac0
Leonidas Christodoulou <l.christodoulou@qobo.biz>
2017-05-05 09:47:14 +0300

    Phpcs fixes (task #3708)

--- webroot/custom-themes/custom/header.php
+++ webroot/custom-themes/custom/header.php
@@ -33,20 +33,20 @@ $header_nav = [
 </head>
 <body <?php body_class(); ?>>
 	<header id="top-header">
-	    <nav id="top-navigation-bar" class="navbar navbar-inverse navbar-fixed-top">
-	      <div class="container">
-	        <div class="navbar-header">
-	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	            <span class="sr-only">Toggle navigation</span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <a class="navbar-brand" href="#">Qobo Generic Theme</a>
-	        </div>
-	        <div id="navbar" class="collapse navbar-collapse navbar-right">
-	        	<?php wp_nav_menu( $header_nav );	?>
-	        </div>
-	      </div>
-	    </nav>
+		<nav id="top-navigation-bar" class="navbar navbar-inverse navbar-fixed-top">
+		  <div class="container">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="#">Qobo Generic Theme</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse navbar-right">
+				<?php wp_nav_menu( $header_nav );	?>
+			</div>
+		  </div>
+		</nav>
 	</header>
