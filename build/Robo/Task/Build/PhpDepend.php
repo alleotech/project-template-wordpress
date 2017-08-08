d6526af4c8fcf1ffec9264fde370627b2e1c9b78
Alexander Mamchenkov <mamtchenkov@gmail.com>
2017-08-08 12:23:19 +0300

    Implemented build tasks (task #4292)
    implemented most of the build tasks
    in a reusable manner.

---
+++ build/Robo/Task/Build/PhpDepend.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Qobo\Robo\Task\Build;
+
+class PhpDepend extends Base
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $taskKey = "pdepend";
+}
---
+++ build/Robo/Task/Build/PhpLoc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Qobo\Robo\Task\Build;
+
+class PhpLoc extends Base
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $taskKey = "phploc";
+}
