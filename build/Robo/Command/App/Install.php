df134c1e7dc11ab7a8e4e722658cb570a85e0c40
Alexander Mamchenkov <mamtchenkov@gmail.com>
2017-08-16 16:40:02 +0300

    Improved App install command (task #4292)
    * Improved stopping on error
    * Moved app:install to app:wp-install
    * Added app:wp-update

--- build/Robo/Command/App/Install.php
+++ build/Robo/Command/App/Install.php
@@ -7,98 +7,222 @@ use \Qobo\Robo\AbstractCommand;
 class Install extends AbstractCommand
 {
     /**
-     * Install a project
+     * Install a wordpress project
      *
      * @return bool true on success or false on failure
      */
-    public function appInstall()
+    public function appWpInstall()
     {
-        $this->preInstall();
+        if (!$this->preInstall()) {
+            return false;
+        }
 
-        $result = $this->taskMysqlDbCreate()
-            ->db(getenv('DB_NAME'))
-            ->user(getenv('DB_ADMIN_USER'))
-            ->pass(getenv('DB_ADMIN_PASS'))
-            ->host(getenv('DB_HOST'))
-            ->run();
+        $result = $this->taskDotenvReload()->run();
+        $data = $result->getData();
+        if (!$result->wasSuccessful() || !isset($data['data'])) {
+            return false;
+        }
 
-        $tokens = $this->taskDotenvReload()->run()['data'];
+        // make sure DB password is either present
+        // or is set to null
+        $dbPass = getenv('DB_ADMIN_PASS');
+        if (empty($dbPass)) {
+            $dbPass = null;
+        }
+
+        $tokens = $data['data'];
         if (!isset($tokens['SYSTEM_COMMAND_WPCLI'])) {
             $tokens['SYSTEM_COMMAND_WPCLI'] = './vendor/bin/wp --allow-root --path=webroot/wp';
         }
-        $result = $this->taskTemplateProcess()
+
+        $result = $this->taskMysqlBaseQuery()
+            ->query("SELECT NOW() AS ServerTime")
+            ->user(getenv('DB_ADMIN_USER'))
+            ->pass($dbPass)
+            ->host(getenv('DB_HOST'))
+            ->run();
+
+        if (!$result->wasSuccessful()) {
+            return false;
+        }
+        $this->say(implode(": ", $result->getData()['data'][0]['output']));
+
+        $tasks = [];
+        $tasks []= $this->taskMysqlDbCreate()
+            ->db(getenv('DB_NAME'))
+            ->user(getenv('DB_ADMIN_USER'))
+            ->pass($dbPass)
+            ->host(getenv('DB_HOST'));
+
+        $tasks []= $this->taskTemplateProcess()
             ->wrap('%%')
             ->tokens($tokens)
             ->src('etc/wp-cli.install')
-            ->dst('etc/wp-cli.install.sh')
-			->run();
+            ->dst('etc/wp-cli.install.sh');
 
-		$result = $this->taskExec('/bin/bash etc/wp-cli.install.sh')->run();
-        $result = $this->taskTemplateProcess()
+        $tasks []= $this->taskExec('/bin/bash etc/wp-cli.install.sh');
+
+        $tasks []= $this->taskTemplateProcess()
             ->wrap('%%')
             ->tokens($tokens)
             ->src('etc/wp-cli.content')
-            ->dst('etc/wp-cli.content.sh')
-			->run();
+            ->dst('etc/wp-cli.content.sh');
 
-		$result = $this->taskExec('/bin/bash etc/wp-cli.content.sh')->run();
+		$tasks []= $this->taskExec('/bin/bash etc/wp-cli.content.sh');
 
-		$result = $this->taskFileChmod()
+		$tasks []= $this->taskFileChmod()
+			->path([getenv('CHMOD_PATH')])
+		->fileMode(0664)
+			->dirMode(0775)
+			->recursive(true);
+
+		$tasks []= $this->taskFileChown()
+			->path([getenv('CHOWN_PATH')])
+			->user(getenv('CHOWN_USER'))
+			->recursive(true);
+
+		$tasks []= $this->taskFileChgrp()
+			->path([getenv('CHGRP_PATH')])
+			->group(getenv('CHGRP_GROUP'))
+			->recursive(true);
+
+        foreach ($tasks as $task) {
+            $result = $task->run();
+            if (!$result->wasSuccessful()) {
+                return false;
+            }
+        }
+
+        return $this->postInstall();
+    }
+
+    /**
+     * Update a wordpress project
+     *
+     * @return bool true on success or false on failure
+     */
+    public function appWpUpdate()
+    {
+        if (!$this->preInstall()) {
+            return false;
+        }
+
+        $result = $this->taskDotenvReload()->run();
+        $data = $result->getData();
+        if (!$result->wasSuccessful() || !isset($data['data'])) {
+            return false;
+        }
+
+        // make sure DB password is either present
+        // or is set to null
+        $dbPass = getenv('DB_ADMIN_PASS');
+        if (empty($dbPass)) {
+            $dbPass = null;
+        }
+
+        $tokens = $data['data'];
+        if (!isset($tokens['SYSTEM_COMMAND_WPCLI'])) {
+            $tokens['SYSTEM_COMMAND_WPCLI'] = './vendor/bin/wp --allow-root --path=webroot/wp';
+        }
+
+        $result = $this->taskMysqlBaseQuery()
+            ->query("SELECT NOW() AS ServerTime")
+            ->user(getenv('DB_ADMIN_USER'))
+            ->pass($dbPass)
+            ->host(getenv('DB_HOST'))
+            ->run();
+
+        if (!$result->wasSuccessful()) {
+            return false;
+        }
+        $this->say(implode(": ", $result->getData()['data'][0]['output']));
+
+        $tasks = [];
+
+        $tasks []= $this->taskTemplateProcess()
+            ->wrap('%%')
+            ->tokens($tokens)
+            ->src('etc/wp-cli.update')
+            ->dst('etc/wp-cli.update.sh');
+
+        $tasks []= $this->taskMysqlDbFindReplace()
+            ->search(getenv('DB_FIND'))
+            ->replace(getenv('DB_REPLACE'))
+            ->db(getenv('DB_NAME'))
+            ->user(getenv('DB_ADMIN_USER'))
+            ->pass($dbPass)
+            ->host(getenv('DB_HOST'));
+
+		$tasks []= $this->taskExec('/bin/bash etc/wp-cli.update.sh');
+
+		$tasks []= $this->taskFileChmod()
 			->path([getenv('CHMOD_PATH')])
 			->fileMode(0664)
 			->dirMode(0775)
-			->recursive(true)
-			->run();
+			->recursive(true);
 
-		$result = $this->taskFileChown()
+		$tasks []= $this->taskFileChown()
 			->path([getenv('CHOWN_PATH')])
 			->user(getenv('CHOWN_USER'))
-			->recursive(true)
-			->run();
+			->recursive(true);
 
-		$result = $this->taskFileChgrp()
+		$tasks []= $this->taskFileChgrp()
 			->path([getenv('CHGRP_PATH')])
 			->group(getenv('CHGRP_GROUP'))
-			->recursive(true)
-			->run();
+			->recursive(true);
 
-        $this->postInstall();
+        foreach ($tasks as $task) {
+            $result = $task->run();
+            if (!$result->wasSuccessful()) {
+                return false;
+            }
+        }
+
+        return $this->postInstall();
     }
 
     protected function preInstall()
     {
-        if (is_file('.env') && is_readable('.env')) {
-            $result = $this->taskDotenvReload()
-               ->path('.env')
-               ->run();
+        if (is_file('.env') && is_readable('.env') && !$this->taskDotenvReload()->path('.env')->run()->wasSuccessful()) {
+            return false;
         }
 
         // old :builder:init
-        $this->versionBackup("build/version");
+        if (!$this->versionBackup("build/version")) {
+            return false;
+        }
 
         $result = $this->taskProjectDotenvCreate() // old :dotenv:create
                 ->env('.env')
                 ->template('.env.example')
                 ->run();
 
+        if (!$result->wasSuccessful()) {
+            return false;
+        }
+
         $result = $this->taskDotenvReload()                // old :dotenv:reload
                 ->path('.env')
                 ->run();
 
+        if (!$result->wasSuccessful()) {
+            return false;
+        }
         $env = $result->getData()['data'];
 
         // old :file:process
-        $result = $this->taskTemplateProcess()
+        return $this->taskTemplateProcess()
             ->wrap('%%')
             ->tokens($env)
             ->src(getenv('TEMPLATE_SRC'))
             ->dst(getenv('TEMPLATE_DST'))
-            ->run();
+            ->run()
+            ->wasSuccessful();
     }
 
     protected function postInstall()
     {
-        $this->versionBackup("build/version.ok");
+        return $this->versionBackup("build/version.ok");
     }
 
     protected function versionBackup($path)
@@ -107,7 +231,7 @@ class Install extends AbstractCommand
         if (file_exists($path)) {
             rename($path, "$path.bak");
         }
-        file_put_contents($path, $projectVersion);
+        return (file_put_contents($path, $projectVersion) === false) ? false : true;
     }
 
     protected function getProjectVersion()
