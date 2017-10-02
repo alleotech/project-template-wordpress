1deb5d7645d768e05f14aa69838839d0c42d3edb
Leonidas Christodoulou <l.christodoulou@qobo.biz>
2017-10-02 16:24:20 +0300

    Unregisters unnesesary inline css code (task #4566)

--- webroot/custom-themes/custom/lib/functions/register-unregister-theme-parts.php
+++ webroot/custom-themes/custom/lib/functions/register-unregister-theme-parts.php
@@ -44,3 +44,37 @@ function unregister_default_sidebars() {
 	unregister_sidebar( 'sidebar-left' );
 	unregister_sidebar( 'sidebar-right' );
 }
+
+/**
+ * Unregisters wp emojicons
+ *
+ * @return void
+ */
+function disable_wp_emojicons() {
+
+	// all actions related to emojis.
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	// filter to remove TinyMCE emojis.
+	add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+}
+
+add_action( 'init', 'disable_wp_emojicons' );
+
+
+/**
+ * Disables contact form 7 inline styles
+ *
+ * @return void
+ */
+function disable_cf7_inline_styles() {
+	remove_action( 'wp_head', 'cf7bs_inline_styles' );
+}
+
+add_action( 'init', 'disable_cf7_inline_styles' );
+
